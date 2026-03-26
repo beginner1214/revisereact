cd10c4040ed14cd9bea3c3bca67ed3dee387d9f0
Aviraj <avirajayuraj1214@gmail.com>
2026-03-26 23:10:52 +0530

    new static

--- js/oops.js
+++ js/oops.js
@@ -2,7 +2,7 @@
 //   let user = {
 //     firstname: this.firstname,
 //     age: this.age,
-  
+
 //   };
 
 //   return user
@@ -12,17 +12,31 @@
 
 // console.log(user1);
 
-
-class Animal{
-  constructor(name){
-    this.name=name;
+class Animal {
+  #age
+  constructor(name,age) {
+    this.name = name;
+    this.#age=age;
+  }
+  #getAge() {
+    return this.#age;
+  }
+  get age() {
+    return this.#getAge();
+  }
+  set age(value) {
+    this.#age = value;
   }
 }
-class Animal2 extends Animal{
-  constructor(name){
-    this.name=name;
+class Animal2 extends Animal {
+  constructor(name,age) {
+    super(name,age);
   }
 }
 
-let d=new Animal2("sampy");
+let hi = "samaltman";
+let d = new Animal2("sampy",455);
 console.log(d);
+console.log(d.age);
+d.age = 500;
+console.log(d.age);
